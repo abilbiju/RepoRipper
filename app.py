8cebe575fcc9a128f4bc98038dfcf471ee4cc068
Abil Biju <128446013+abilbiju@users.noreply.github.com>
2025-07-19 07:46:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -201,5 +201,5 @@ def roast_user():
     return jsonify(result)
 
 if __name__ == '__main__':
-    port== int(os.environ.get("PORT", 5000))
+    port= int(os.environ.get("PORT", 5000))
     app.run(debug=True,host='0.0.0.0',port=port)
